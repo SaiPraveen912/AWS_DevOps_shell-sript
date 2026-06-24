87c29ab1a186f76fb1098108be1af3990d520c9e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-06-24 11:40:02 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Declaring of variable
 PERSON1=Sachin # no space between = and value
 PERSON2=Rahul
 
+# refferring the variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, How are you"
 echo "$PERSON1:: I am fine too. how is your work?"
