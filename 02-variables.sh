5ef4fa1de8a2cb023cba7a3f6525852839f34016
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-06-24 11:24:41 +0530

    variables-problem-shell-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is again sample script
+#!/bin/bash
+
+echo "Sai:: Hello Praveen, How are you?"
+echo "Praveen:: Hello Sai, How are you"
+echo "Sai:: I am fine too. how is your work?"
+echo "Praveen:: Not bad, I am thinking to upgrade to DevOps"
