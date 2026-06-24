7529bd05fbec989fda2d908b65dd00023210c24f
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-06-24 13:01:58 +0530

    variables-passed-as-argument

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Declaring of variable
+PERSON1=$1 # no space between = and value
+PERSON2=$2
+
+# refferring the variable
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, How are you"
+echo "$PERSON1:: I am fine too. how is your work?"
+echo "$PERSON2:: Not bad, I am thinking to upgrade to DevOps"
