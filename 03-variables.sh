85546396c784b9ef0f3d7f458c1096c230aaa0d9
SaiPraveen912 <saipraveen.immanni@gmail.com>
2026-06-26 10:47:48 +0530

    variable and arrays in shell scripting

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,6 +6,6 @@ PERSON2=Rahul
 
 # refferring the variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, How are you"
+echo "${PERSON2}:: Hello $PERSON1, How are you"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: Not bad, I am thinking to upgrade to DevOps"
---
+++ 06-array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Array is list of values
+
+# List of movies
+MOVIES=("RRR" "DjTillu" "Murari")
+
+# List always start with 0
+# Size of above array is 3
+# indexes are 0, 1, 2
+
+echo "First movie is: ${MOVIES[0]}"
